c79314498e6744726f27c9a9776719b8db8079d1
Ali Taherzadeh <Ali.Thz83@gmail.com>
2025-04-23 19:01:09 +0330

    add transportation search api

--- alibabaclone-frontend/src/shared/api/agent.ts
+++ alibabaclone-frontend/src/shared/api/agent.ts
@@ -1,8 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
+import { TransportationSearchRequest } from '../models/transportation/transportationSearchRequest';
+// import { store } from '../stores/store';
 
 axios.defaults.baseURL = 'https://localhost:7028/api';
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+// axios.interceptors.request.use(config => {
+//     const token = store.commonStore.token;
+//     if (token && config.headers) {
+//         config.headers.Authorization = `bearer ${token}`;
+//     }
+//     return config;
+// });
 
 const request = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
@@ -11,8 +21,22 @@ const request = {
     delete: <T>(url: string) => axios.delete<T>(url).then(responseBody)
 }
 
-const agent = {
+const transportationSearchRequests = {
+    list: () => request.get<TransportationSearchRequest[]>('/activities'),
+    details: (id: string) => request.get<TransportationSearchRequest>(`/activities/${id}`),
+    create: (transportationSearchRequest: TransportationSearchRequest) => request.post<void>(`/activities`, transportationSearchRequest),
+    update: (transportationSearchRequest: TransportationSearchRequest) => request.put<void>(`/activities/`, transportationSearchRequest),
+    delete: (id: string) => request.delete<void>(`/activities/${id}`)
+}
 
+// const Account = {
+//     current: () => request.get<User>('/account'),
+//     login: (user: UserFormValues) => request.post<User>('/account/login', user),
+//     register: (user: UserFormValues) => request.post<User>('/account/register', user)
+// }
+
+const agent = {
+    transportationSearchRequests
 }
 
 export default agent;
